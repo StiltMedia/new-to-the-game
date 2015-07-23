af7cba2a5f7a4d6846bb02814594909ca244348c
Mark Natera <marknatera@gmail.com>
2015-07-23 10:36:48 -0400

    Added all files from the theme

--- app/assets/javascripts/application-btm.js
+++ app/assets/javascripts/application-btm.js
@@ -1,1 +1,34 @@
 //= require bootstrap-sprockets
+//= require jquery.appear
+//= require waypoint
+//= require modernizr-latest
+//= require jquery.easing.1.3
+//= require SmoothScroll
+//= require jquery.magnific-popup.min
+//= require jquery.superslides
+//= require jquery.flexslider
+//= require jquery.simple-text-rotator
+//= require jquery.cubeportfolio
+//= require owl.carousel.min
+//= require jquery.parallax-1.1.3
+//= require skrollr.min
+//= require jquery.fitvids
+//= require jquery.mb.YTPlayer
+
+// Revolution Slider
+//= require jquery.themepunch.revolution.min
+//= require jquery.themepunch.tools.min
+//= require rev_plugins
+
+// Twitter
+//= require tweecool.min
+//= require tweecool
+
+// Page Plugins
+//= require plugins
+
+// Portfolio Plugins
+//= require portfolio
+
+// Theme Panel
+//= require themepanel
